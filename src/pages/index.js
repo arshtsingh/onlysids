bc5922a23cf81dd1cc4c56a67bfc725b8b043c4c
arshy <arshgnis@gmail.com>
2021-03-31 19:50:05 -0500

    refactoring

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,22 @@
 import Head from 'next/head'
-import 'tailwindcss/tailwind.css'
-import React, { Component } from 'react'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <>
+      <h1>only sids</h1>
       <div>
-        <h1>only sids</h1>
         <Image
           src="/sid-header.jpg"
           alt="Picture of the sid"
           width={1000}
           height={500}
         />
-        <p>10 bucks a pop</p>
+      </div>
+      <div className="flex flex-wrap justify-center">
+        <div className="w-6/12 sm:w-4/12 px-4">
+          <img src="./public/sid-face.JPG" alt="..." className="shadow rounded-full max-w-full h-auto align-middle border-none" />
+        </div>
       </div>
     </>
   )
